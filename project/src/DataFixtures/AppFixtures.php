aaf12a93adee4415d820941df148f49fa007b247
Hai Yassin <haiyassin1701@gmail.com>
2020-11-29 15:22:53 +0100

    Set up Fixtures

--- project/src/DataFixtures/AppFixtures.php
+++ project/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Token;
+use App\Entity\User;
+use App\Service\TokenService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /** @var TokenService  */
+    private TokenService $tokenService;
+
+    public function __construct(TokenService $tokenService)
+    {
+        $this->tokenService = $tokenService;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 4; $i++) {
+
+            $token = new Token();
+            $token->setCode($this->tokenService->tokenCode());
+
+            $manager->persist($token);
+
+            $user = new User();
+            $user
+                ->setEmail("email$i@fixture.fr")
+                ->setPassword("password$i")
+                ->addToken($token)
+            ;
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
