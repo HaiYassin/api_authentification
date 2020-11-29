571dc6b2f3c7529ed0a254cc062be439fdf5cd67
Hai Yassin <haiyassin1701@gmail.com>
2020-11-29 15:29:38 +0100

    Add some change on tests/MockHelperTrait

--- project/tests/MockHelperTrait.php
+++ project/tests/MockHelperTrait.php
@@ -4,7 +4,28 @@
 namespace Tests;
 
 
-class MockHelperTrait
-{
+use PHPUnit\Framework\MockObject\MockObject;
 
+trait MockHelperTrait
+{
+    /**
+     * @param $className
+     * @param $methodReturnValues
+     *
+     * @return MockObject
+     */
+    public function getMockObject($className, $methodReturnValues)
+    {
+        $mockObject = $this->getMockBuilder($className)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        foreach ($methodReturnValues as $methodName => $returnValue) {
+            $mockObject->expects($this->any())
+                ->method($methodName)
+                ->willReturn($returnValue);
+        }
+
+        return $mockObject;
+    }
 }
